562d8a30dbbce9bcb95b4139e389f3dac6882103
ogabriel <gabrieloliver8991@gmail.com>
2020-04-15 22:40:18 -0300

    basic configuration of express

--- server.js
+++ server.js
@@ -2,3 +2,12 @@
 
 // Config envs
 require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server running at port ${PORT}`)
+})
