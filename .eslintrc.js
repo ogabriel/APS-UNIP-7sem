5b93727b04fd9a8d4535326da306f8d1471029ee
ogabriel <gabrieloliver8991@gmail.com>
2020-04-20 12:41:48 -0300

    remvoe commonjs flag

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es6: true,
-    jest: true
+    jest: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
