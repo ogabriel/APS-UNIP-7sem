27825fb9f5c92ce7612b7522828f46f8356a9b22
ogabriel <gabrieloliver8991@gmail.com>
2020-04-21 15:41:36 -0300

    remove app configurations from app

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 'use strict';
 
-// Config envs
-require('dotenv').config();
+const app = require('./app');
 
-const path = require('path');
-const routes = require('./config/routes');
-const express = require('express');
-const app = express();
-
-// Config static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Config api routes
-app.use('/api/v1', routes);
-
-// Config server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
