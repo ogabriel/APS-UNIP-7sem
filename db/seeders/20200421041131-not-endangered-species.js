9112dd9af9ddc21a67ae6c2fb6f12bc5a65a7d9c
ogabriel <gabrieloliver8991@gmail.com>
2020-04-21 01:19:17 -0300

    add seeds for species

---
+++ db/seeders/20200421041131-not-endangered-species.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Species',
+      [
+        {
+          popularName: 'Laranjeira',
+          scientificName: 'Citrus sinensis',
+          endangered: false,
+          description: 'O fruto da árvore é a laranja',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          popularName: 'Macieira',
+          scientificName: 'Malus domestica',
+          endangered: false,
+          description: 'O fruto da árvore é a maça',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(
+      'Species',
+      [
+        { scientificName: 'Citrus sinensis' },
+        { scientificName: 'Malus domestica' },
+      ],
+      {}
+    );
+  },
+};
---
+++ db/seeders/20200421041144-endangered-species.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Species',
+      [
+        {
+          popularName: 'Araucária Chilena',
+          scientificName: 'Araucaria araucana',
+          endangered: true,
+          description: 'Araucaria nativa do Chile',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          popularName: 'Dioneia',
+          scientificName: 'Dionaea muscipula',
+          endangered: true,
+          description: 'Planta carnírvora que come insetos e aracnídeos',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(
+      'Species',
+      [
+        { scientificName: 'Araucaria araucana' },
+        { scientificName: 'Dionaea muscipula' },
+      ],
+      {}
+    );
+  },
+};
