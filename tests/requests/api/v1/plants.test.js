b9a6ee4e4e5cb6d54081ba4fa82d7a5356362b81
ogabriel <gabrieloliver8991@gmail.com>
2020-04-23 12:09:04 -0300

    add coordenates checking

--- tests/requests/api/v1/plants.test.js
+++ tests/requests/api/v1/plants.test.js
@@ -48,6 +48,15 @@ describe('GET /plants/localization', () => {
       expect(response.body.length).toEqual(1);
       expect(response.body[0].type).toEqual('Feature');
       expect(response.body[0].properties).not.toEqual({});
+      expect(response.body[0].geometry.type).toEqual('Point');
+      expect(response.body[0].geometry.coordinates[0]).toBeGreaterThanOrEqual(
+        -90
+      );
+      expect(response.body[0].geometry.coordinates[0]).toBeLessThanOrEqual(90);
+      expect(response.body[0].geometry.coordinates[1]).toBeGreaterThanOrEqual(
+        -180
+      );
+      expect(response.body[0].geometry.coordinates[1]).toBeLessThanOrEqual(180);
     });
   });
 });
