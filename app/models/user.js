f91d389164747f22b8ae9a9110aa5efc1ef59731
Breno Lipi <brenorlipi@hotmail.com>
2021-01-04 16:14:31 -0300

    User model additions

--- app/models/user.js
+++ app/models/user.js
@@ -3,35 +3,33 @@
 const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notEmpty: true,
-        },
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
       },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isEmail: true,
-        },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
       },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        set(plainPassword) {
-          this.setDataValue('password', bcrypt.hashSync(plainPassword, 10));
-        },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(plainPassword) {
+        this.setDataValue('password', bcrypt.hashSync(plainPassword, 10));
       },
-      company: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-      },
-    });
+    },
+    company: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
 
   User.associate = function (models) {
     User.hasMany(models.Plant);
@@ -40,6 +38,6 @@ module.exports = (sequelize, DataTypes) => {
   User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
-  
+
   return User;
 };
